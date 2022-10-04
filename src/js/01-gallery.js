1326841b6c7b82e34ae996db6894663badbb7116
jkl303 <303vladzoz@gmail.com>
2022-10-04 14:11:30 +0200

    completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,16 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const imgsMarkup = galleryItems.map(({preview, original, description}) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join('');
+gallery.insertAdjacentHTML('beforeend', imgsMarkup);
+gallery.addEventListener('click', e => e.preventDefault());
+let galleryLightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
-
+import throttle from "lodash.throttle";
+const STG_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+form.addEventListener("input", throttle(onInput, 500));
+function onInput() {
+    localStorage.setItem(STG_KEY, JSON.stringify({ email: email.value, message: message.value }))
+};
+if (localStorage.getItem(STG_KEY)) {
+    const parsed = JSON.parse(localStorage.getItem(STG_KEY));
+    email.value = parsed.email;
+    message.value = parsed.message;
+}
+form.addEventListener("submit", onSubmit);
+function onSubmit(e) {
+    e.preventDefault();
+    const parsed = JSON.parse(localStorage.getItem(STG_KEY));
+    if (parsed) {
+        console.log(parsed);
+    };
+    email.value = '';
+    message.value = '';
+    localStorage.clear();
+}
